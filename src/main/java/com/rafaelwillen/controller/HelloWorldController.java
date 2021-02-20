4d04afe3399f918751668ea5adf42e3f5e5e513b
Rafael Padre <rafaelpadre20@gmail.com>
2021-02-20 13:52:50 +0100

    Made a first run to see if everything is working

---
+++ src/main/java/com/rafaelwillen/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.rafaelwillen.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloWorldController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("Hello World");
+    }
+}
