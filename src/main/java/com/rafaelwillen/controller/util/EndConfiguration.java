00c35cffa0c09d1eaa930641a347b5f7ed5d868e
Rafael Padre <rafaelpadre20@gmail.com>
2021-02-22 15:26:32 +0100

    Created the end configuration screen

--- src/main/java/com/rafaelwillen/controller/util/EndConfiguration.java
+++ src/main/java/com/rafaelwillen/controller/util/EndConfiguration.java
@@ -1,2 +1,65 @@
-package com.rafaelwillen.controller.util;public class EndConfiguration {
+package com.rafaelwillen.controller.util;
+
+import com.rafaelwillen.database.DatabaseManager;
+import com.rafaelwillen.util.AlertManager;
+import com.rafaelwillen.util.CustomWindow;
+import com.rafaelwillen.util.RoutesConstants;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class EndConfiguration extends CustomWindow implements Initializable {
+
+    @FXML
+    private AnchorPane topBar;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            DatabaseManager.setSetupComplete();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            AlertManager.showErrorAlert("Erro na base de dados", "Erro na base de dados. Tente novamente");
+        }
+        makeWindowDraggable(topBar);
+    }
+
+    @FXML
+    void closeWindow(MouseEvent event) {
+        nextPage(null);
+    }
+
+    @FXML
+    void nextPage(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource(RoutesConstants.LOGIN_FXML));
+            Stage stage = new Stage(StageStyle.UNDECORATED);
+            stage.setScene(new Scene(root));
+            stage.setTitle("Login");
+            stage.setResizable(false);
+            stage.show();
+            closeWindow(topBar, true);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            AlertManager.showErrorAlert("Erro Fatal", "Ocorreu um erro fatal. Por favor reinstala a aplicacao");
+        }
+
+    }
+
+    @Override
+    protected boolean validateForm() {
+        return false;
+    }
 }
--- src/main/java/com/rafaelwillen/util/RoutesConstants.java
+++ src/main/java/com/rafaelwillen/util/RoutesConstants.java
@@ -2,12 +2,20 @@ package com.rafaelwillen.util;
 
 public class RoutesConstants {
 
-    private final static String PROJECT_ROOT = "/com/rafaelwillen/";
+    private final static String PROJECT_ROOT = "/com/rafaelwillen/fxml/";
     // Family package create forms
-    public final static String CREATE_FAMILY_FXML = PROJECT_ROOT + "fxml/form/createFamily.fxml";
-    public final static String CREATE_SON_FXML = PROJECT_ROOT + "fxml/form/createSon.fxml";
-    public final static String CREATE_PARENT_FXML = PROJECT_ROOT + "fxml/form/createParent.fxml";
-    public final static String CREATE_PET_FXML = PROJECT_ROOT + "fxml/form/createPet.fxml";
+    public final static String CREATE_FAMILY_FXML = PROJECT_ROOT + "form/createFamily.fxml";
+    public final static String CREATE_SON_FXML = PROJECT_ROOT + "form/createSon.fxml";
+    public final static String CREATE_PARENT_FXML = PROJECT_ROOT + "form/createParent.fxml";
+    public final static String CREATE_PET_FXML = PROJECT_ROOT + "form/createPet.fxml";
+
+    // Other forms
+    public final static String LOGIN_FXML = PROJECT_ROOT + "form/login.fxml";
+
+    // Utilities views
+    public final static String WELCOME_SCREEN_FXML = PROJECT_ROOT + "utils/welcome.fxml";
+    public final static String END_CONFIGURATION_FXML = PROJECT_ROOT + "utils/endConfiguration.fxml";
+
 
     private RoutesConstants() {
     }
